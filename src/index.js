85256c8d6761edb896f63deb04cae873ea6a858c
tenor2000 <tenor2000@gmail.com>
2025-03-12 16:01:56 -0400

    removed loop so updates will show

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 // Create your game here!
 const app = document.getElementById("app");
 app.style.display = "flex";
+app.style.flexDirection = "column";
 app.style.justifyContent = "center";
-
 app.style.alignItems = "center";
-app.style.height = "100vh";
-app.style.width = "100vw";
 const newEl = (el) => document.createElement(el);
+
+const directions = newEl("h1");
+const subtitle = newEl("p");
+subtitle.style.marginTop = "0";
+directions.textContent = "Guess the Number!";
+subtitle.textContent = `Or not, I'm not your mom or anything...`;
+
+app.appendChild(directions);
+app.appendChild(subtitle);
 
 const startButton = newEl("button");
 startButton.textContent = "Start";
@@ -18,43 +25,17 @@ startButton.addEventListener("click", () => {
 function playGame(size) {
   let userGuesses = [];
   const mysteryNum = Math.floor(Math.random() * size) + 1;
+  const gameboard = newEl("table");
+  app.appendChild(gameboard);
 
-  while (true) {
-    createGameboard();
-    console.log("Looping");
+  startButton.remove();
 
-    const userGuess = promptUser();
-    const guessInt = parseInt(userGuess);
-
-    if (isNaN(guessInt)) {
-      alert(`${userGuess} is not a number`);
-      continue;
-    } else if (guessInt < 1 || guessInt > size) {
-      alert(`Your guess: ${userGuess} is not in the range`);
-      continue;
-    }
-
-    if (guessInt === mysteryNum) {
-      alert(`You have guessed the correct answer`);
-      clearGameboard();
-      break;
-    } else if (guessInt > mysteryNum) {
-      alert("Lower");
-    } else if (guessInt < mysteryNum) {
-      alert("Higher");
-    }
-
-    userGuesses.push(parseInt(userGuess));
-
-    clearGameboard();
-  }
-
-  createGameboard(true);
+  createGameboard();
+  const guessButton = createGuessButton();
+  app.appendChild(guessButton);
 
   function createGameboard(victory = false) {
     const xySize = Math.sqrt(size);
-
-    const gameboard = newEl("table");
 
     let curr = 1;
 
@@ -74,20 +55,62 @@ function playGame(size) {
       }
       gameboard.appendChild(row);
     }
-
-    app.appendChild(gameboard);
   }
 
   function clearGameboard() {
-    while (app.firstChild) {
-      app.removeChild(app.firstChild);
+    while (gameboard.firstChild) {
+      gameboard.removeChild(gameboard.firstChild);
     }
   }
 
   function promptUser() {
     const question = `Enter Number Value Between 1 and ${size}: `;
     const guess = window.prompt(question, 0);
+    const guessInt = parseInt(guess);
 
-    return guess;
+    if (isNaN(guessInt)) {
+      alert(`${guess} is not a number`);
+    } else if (guessInt < 1 || guessInt > size) {
+      alert(`Your guess (${guess}) is not in the range.`);
+      return false;
+    }
+
+    if (guessInt === mysteryNum) {
+      clearGameboard();
+      createGameboard(true);
+      app.removeChild(guessButton);
+      app.appendChild(tryAgainButton());
+      alert("You have chosen the correct number!");
+      return;
+    } else if (guessInt > mysteryNum) {
+      alert("Lower");
+    } else if (guessInt < mysteryNum) {
+      alert("Higher");
+    }
+
+    userGuesses.push(parseInt(guess));
+
+    clearGameboard();
+    createGameboard();
+  }
+
+  function createGuessButton() {
+    const button = newEl("button");
+    button.textContent = "Make a Guess";
+    button.style.marginTop = "1rem";
+    button.addEventListener("click", () => {
+      promptUser();
+    });
+    return button;
+  }
+
+  function tryAgainButton() {
+    const tryAgain = newEl("button");
+    tryAgain.textContent = "Try Again";
+    tryAgain.style.marginTop = "1rem";
+    tryAgain.addEventListener("click", () => {
+      location.reload();
+    });
+    return tryAgain;
   }
 }
